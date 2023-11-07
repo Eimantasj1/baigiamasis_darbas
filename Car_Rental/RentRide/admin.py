023073c6e176e95e3949cfa6dbb35514545f0b62
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-11-07 09:56:25 +0200

    base/index

--- Car_Rental/RentRide/admin.py
+++ Car_Rental/RentRide/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from .models import Car, Rental
+from .models import Location, CarDealer, Car, Customer, Order
 
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('brand', 'model', 'year', 'price_per_day', 'is_available')
-    list_filter = ('is_available',)
-    search_fields = ('brand', 'model', 'year')
-    prepopulated_fields = {'car_image': ('brand', 'model')}
-
-@admin.register(Rental)
-class RentalAdmin(admin.ModelAdmin):
-    list_display = ('car', 'user', 'reservation_date', 'return_date')
-    list_filter = ('car', 'user')
-    search_fields = ('car__brand', 'car__model', 'user__username')
+admin.site.register(Location)
+admin.site.register(CarDealer)
+admin.site.register(Car)
+admin.site.register(Customer)
+admin.site.register(Order)
--- Car_Rental/RentRide/models.py
+++ Car_Rental/RentRide/models.py
@@ -1,64 +1,54 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-from datetime import date
-from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinLengthValidator, MaxLengthValidator, FileExtensionValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-User = get_user_model()
+class Location(models.Model):
+    city = models.CharField(max_length=50, primary_key=True)
+
+    def __str__(self):
+        return self.city
+
+class CarDealer(models.Model):
+    car_dealer = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone = models.CharField(validators=[MinLengthValidator(10), MaxLengthValidator(10)], max_length=10)
+    location = models.OneToOneField(Location, on_delete=models.PROTECT)
+    earnings = models.IntegerField(default=0)
+    type = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.car_dealer)
 
 def validate_car_image(value):
-    if not value:
-        raise ValidationError("Image is required for a car.")
+    if not value.name.endswith(('.jpg', '.jpeg', '.png')):
+        raise models.ValidationError("Only JPG, JPEG, and PNG images are allowed.")
 
 class Car(models.Model):
-    brand = models.CharField(max_length=50, db_index=True)
-    model = models.CharField(max_length=50)
-    year = models.PositiveSmallIntegerField()
-    price_per_day = models.DecimalField(max_digits=8, decimal_places=2)
-    car_image = models.ImageField(upload_to='car_images/', null=True, blank=True, validators=[validate_car_image])
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="car_images/", validators=[validate_car_image])
+    car_dealer = models.ForeignKey(CarDealer, on_delete=models.PROTECT)
+    capacity = models.PositiveSmallIntegerField()
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     is_available = models.BooleanField(default=True)
-    notes = models.TextField(max_length=500, null=True, blank=True)
-
-    class Meta:
-        verbose_name = _("car")
-        verbose_name_plural = _("cars")
+    rent = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
-        return f"{self.brand} {self.model} ({self.year})"
+        return self.name
 
-    def get_absolute_url(self):
-        return reverse("car_detail", kwargs={"pk": self.pk})
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone = models.CharField(validators=[MinLengthValidator(10), MaxLengthValidator(10)], max_length=10)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, blank=True, null=True)
 
-class Rental(models.Model):
+    def __str__(self):
+        return str(self.user)
+
+class Order(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    car_dealer = models.ForeignKey(CarDealer, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    reservation_date = models.DateField()
-    return_date = models.DateField()
-
-    class Meta:
-        verbose_name = _("rental")
-        verbose_name_plural = _("rentals")
-
-    def save(self, *args, **kwargs):
-        today = date.today()
-        if self.reservation_date < today:
-            raise ValidationError(_("Reservation date cannot be earlier than today."))
-        elif self.return_date <= self.reservation_date:
-            raise ValidationError(_("Return date cannot be earlier than or the same as reservation date."))
-        
-        rental_for_car = Rental.objects.filter(car=self.car, return_date__gte=self.reservation_date, reservation_date__lte=self.return_date)
-        if rental_for_car.exists():
-            raise ValidationError(_("This car is already booked for the selected dates."))
-        
-        self.car.is_available = False
-        self.car.save()
-        super().save(*args, **kwargs)
-
-    def delete(self, *args, **kwargs):
-        self.car.is_available = True
-        self.car.save()
-        super().delete(*args, **kwargs)
-
-    def __str__(self):
-        return f"Rented {self.car} by {self.user.username}"
+    rent = models.CharField(max_length=10)
+    days = models.PositiveSmallIntegerField()
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
