a61f1f4e8c9cdf25fd69f359a7e8cc8d076f7c8f
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-11-07 15:30:27 +0200

    signup ir logout

--- Car_Rental/RentRide/urls.py
+++ Car_Rental/RentRide/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path("customer_login/", views.customer_login, name="customer_login"),
+    path("customer_signup/", views.customer_signup, name="customer_signup"),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Car_Rental/RentRide/views.py
+++ Car_Rental/RentRide/views.py
@@ -3,9 +3,56 @@ from . models import *
 from django.contrib.auth import authenticate, login, logout
 
 
-
 def index(request):
     return render(request, "index.html")
+
+def customer_signup(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        phone = request.POST['phone']
+        city = request.POST['city']
+
+        if password1 != password2:
+            return redirect("/customer_signup")
+
+        # Create or get the User object
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={
+                'email': email,
+                'first_name': first_name,
+                'last_name': last_name,
+            }
+        )
+        if created:
+            user.set_password(password1)  # Set the password for the new user
+            user.save()
+
+        # Create or get the Location object
+        location, created = Location.objects.get_or_create(city=city.lower())
+
+        # Create the Customer object
+        customer, created = Customer.objects.get_or_create(
+            user=user,
+            defaults={'phone': phone, 'location': location, 'type': "Customer"}
+        )
+
+        # Automatically log in the user after registration
+        login(request, user)
+
+        alert = True
+        return render(request, "customer_signup.html", {'alert': alert})
+
+    return render(request, "customer_signup.html")
+
 
 def customer_login(request):
     if request.user.is_authenticated:
@@ -25,3 +72,7 @@ def customer_login(request):
                 alert = True
                 return render(request, "customer_login.html", {'alert':alert})
     return render(request, "customer_login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
