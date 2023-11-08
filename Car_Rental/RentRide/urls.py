a8a222bac622636f97443c5c80d24b42d8f62c58
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-11-08 15:19:51 +0200

    dealer login ir smulkus pakeitimai

--- Car_Rental/RentRide/urls.py
+++ Car_Rental/RentRide/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("customer_login/", views.customer_login, name="customer_login"),
     path("customer_signup/", views.customer_signup, name="customer_signup"),
     path('logout/', views.logout_view, name='logout'),
-    path("customer_homepage/", views.customer_homepage, name="customer_homepage")
+    path("customer_homepage/", views.customer_homepage, name="customer_homepage"),
+    path("search_results/", views.search_results, name="search_results"),
+    path("car_rent/", views.car_rent, name="car_rent"),
+    path("car_dealer_login/", views.car_dealer_login, name="car_dealer_login"),
+    path("car_dealer_signup/", views.car_dealer_signup, name="car_dealer_signup"),
 ]
--- Car_Rental/RentRide/views.py
+++ Car_Rental/RentRide/views.py
@@ -23,7 +23,6 @@ def customer_signup(request):
         if password1 != password2:
             return redirect("/customer_signup")
 
-        # Create or get the User object
         user, created = User.objects.get_or_create(
             username=username,
             defaults={
@@ -33,19 +32,16 @@ def customer_signup(request):
             }
         )
         if created:
-            user.set_password(password1)  # Set the password for the new user
+            user.set_password(password1)
             user.save()
 
-        # Create or get the Location object
         location, created = Location.objects.get_or_create(city=city.lower())
 
-        # Create the Customer object
         customer, created = Customer.objects.get_or_create(
             user=user,
             defaults={'phone': phone, 'location': location, 'type': "Customer"}
         )
 
-        # Automatically log in the user after registration
         login(request, user)
 
         alert = True
@@ -79,3 +75,73 @@ def logout_view(request):
 
 def customer_homepage(request):
     return render(request, "customer_homepage.html")
+
+def search_results(request):
+    city = request.POST['city']
+    city = city.lower()
+    vehicles_list = []
+    location = Location.objects.filter(city = city)
+    for a in location:
+        cars = Car.objects.filter(location=a)
+        for car in cars:
+            if car.is_available == True:
+                vehicle_dictionary = {'name':car.name, 'id':car.id, 'image':car.image.url, 'city':car.location.city,'capacity':car.capacity}
+                vehicles_list.append(vehicle_dictionary)
+    request.session['vehicles_list'] = vehicles_list
+    return render(request, "search_results.html")
+
+def car_rent(request):
+    id = request.POST['id']
+    car = Car.objects.get(id=id)
+    cost_per_day = int(car.rent)
+    return render(request, 'car_rent.html', {'car':car, 'cost_per_day':cost_per_day})
+
+def car_dealer_login(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                user1 = CarDealer.objects.get(car_dealer=user)
+                if user1.type == "Car Dealer":
+                    login(request, user)
+                    return redirect("/all_cars")
+                else:
+                    alert = True
+                    return render(request, "car_dealer_login.html", {"alert":alert})
+    return render(request, "car_dealer_login.html")
+
+def car_dealer_signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        city = request.POST['city']
+        phone = request.POST['phone']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            return redirect('/car_dealer_signup')
+
+        user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password1)
+        user.save()
+        try:
+            location = Location.objects.get(city = city.lower())
+        except:
+            location = None
+        if location is not None:
+            car_dealer = CarDealer(car_dealer=user, phone=phone, location=location, type="Car Dealer")
+        else:
+            location = Location(city = city.lower())
+            location.save()
+            location = Location.objects.get(city = city.lower())
+            car_dealer = CarDealer(car_dealer = user, phone=phone, location=location, type="Car Dealer")
+        car_dealer.save()
+        return render(request, "car_dealer_login.html")
+    return render(request, "car_dealer_signup.html")
